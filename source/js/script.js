40d7ffecaf4ac08f213510d8ab9055fe38cd3232
Konstantin <3304040@gmail.com>
2018-05-11 11:17:19 +0300

    Изменения

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,18 @@
-var Body = document.querySelector("body");
-var Button = document.querySelector(".navigation__toggle");
-var List_status = document.querySelector(".navigation__inner--opened");
-Body.classList.remove("no-js");
-Body.classList.add("js");
-List_status.classList.toggle("navigation__inner--closed");
-Button.addEventListener("click", function (evt) {
+var body = document.querySelector("body");
+var nav = document.querySelector(".navigation");
+var navToggle = nav.querySelector(".navigation__toggle");
+var navList = nav.querySelector(".navigation__list");
+
+body.classList.remove("no-js");
+body.classList.add("js");
+
+navToggle.classList.add("navigation__toggle--off");
+navList.classList.add("navigation__list--off");
+
+navToggle.addEventListener("click", function (evt) {
   evt.preventDefault();
-  List_status.classList.toggle("navigation__inner--closed");
+  navToggle.classList.toggle("navigation__toggle--off");
+  navList.classList.toggle("navigation__list--off");
 });
 
 function initialize() {
@@ -15,7 +21,7 @@ function initialize() {
     center: new google.maps.LatLng(((screen.width < 1300) ? 59.93877121663107 : 59.939075), ((screen.width < 1300) ? 30.323274149999975 : 30.31965))
   };
   var map = new google.maps.Map(document.getElementById("mapid"), mapOptions);
-  var image =  "/img/map-pin.png";
+  var image = "img/map-pin.png";
   var myLatLng = new google.maps.LatLng(59.93877121663107, 30.323274149999975);
   var beachMarker = new google.maps.Marker({
     position: myLatLng,
