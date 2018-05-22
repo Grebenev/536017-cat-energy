9f89fd0669c6f4cc24f5ce1422c90fd207d644eb
Konstantin <3304040@gmail.com>
2018-05-22 18:59:55 +0300

    Исправления

--- source/js/script.js
+++ source/js/script.js
@@ -2,8 +2,6 @@ var body = document.querySelector("body");
 var nav = document.querySelector(".navigation");
 var navToggle = nav.querySelector(".navigation__toggle");
 var navList = nav.querySelector(".navigation__list");
-var ScreenTablet = 768;
-var ScreenDeskop = 1300;
 
 body.classList.remove("no-js");
 body.classList.add("js");
@@ -17,22 +15,73 @@ navToggle.addEventListener("click", function (evt) {
   navList.classList.toggle("navigation__list--off");
 });
 
-function initialize() {
-  var mapOptions = {
-    zoom: 17,
-    center: new google.maps.LatLng(((screen.width < ScreenDeskop) ? 59.93877121663107 : 59.939075), ((screen.width < ScreenDeskop) ? 30.323274149999975 : 30.31965))
-  };
-  var map = new google.maps.Map(document.getElementById("mapid"), mapOptions);
-  if(window.screen.width <= ScreenTablet) {
-    var image = "img/map-small-pin.png";
-  } else {
-    var image = "img/map-pin.png";
+var bigPin = {width: 124, height: 106};
+var smallPin = {width: 62, height: 53};
+
+function initMap() {
+  if(document.body.clientWidth < 768) {
+    var mapCenter = {lat: 59.938800, lng: 30.323200};
+    var pinCenter = {lat: 59.938800, lng: 30.323200};
+    var iconSize = smallPin;
+
+  } else if (document.body.clientWidth >= 768 && document.body.clientWidth < 1300) {
+    var mapCenter = {lat: 59.938800, lng: 30.323200};
+    var pinCenter = {lat: 59.938800, lng: 30.323200};
+    var iconSize = bigPin;
+
+  } else if (document.body.clientWidth >= 1300) {
+    var mapCenter = {lat: 59.939065, lng: 30.319335};
+    var pinCenter = {lat: 59.938800, lng: 30.323200};
+    var iconSize = bigPin;
   }
-  var myLatLng = new google.maps.LatLng(59.93877121663107, 30.323274149999975);
-  var beachMarker = new google.maps.Marker({
-    position: myLatLng,
+
+  var map = new google.maps.Map(document.getElementById('mapid'), {
+    zoom: 17,
+    center: mapCenter
+  });
+
+  var image = new google.maps.MarkerImage(
+    'img/map-pin.png',
+    null,
+    null,
+    null,
+    iconSize
+  );
+
+  var marker = new google.maps.Marker({
+    position: pinCenter,
     map: map,
     icon: image
   });
+
+  google.maps.event.addDomListener(window, 'resize', function() {
+    if(document.body.clientWidth >= 768) {
+      marker.setIcon(
+        new google.maps.MarkerImage(
+          marker.getIcon().url,
+          null,
+          null,
+          null,
+          bigPin
+        )
+      );
+    } else {
+      marker.setIcon(
+        new google.maps.MarkerImage(
+          marker.getIcon().url,
+          null,
+          null,
+          null,
+          smallPin
+        )
+      );
+    }
+
+    if(document.body.clientWidth >= 1300) {
+      map.setCenter(new google.maps.LatLng(59.939065, 30.319335));
+    } else {
+      map.setCenter(new google.maps.LatLng(59.938800, 30.323200));
+    }
+  });
 }
-google.maps.event.addDomListener(window, "load", initialize);
+google.maps.event.addDomListener(window, "load", initMap);
